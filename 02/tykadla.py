beee2081beb181f14871c7375d68ad814db2a426
Patricie Hermanová <patriciehermanova@gmail.com>
2025-11-11 20:06:29 +0100

    Delete 02 directory

--- 02/tykadla.py
+++
@@ -1,9 +0,0 @@
-# V1 muze zobrazit vice zprav pri splneni podminek (2x if)
-pocet_tykadel = float(input("Zadej počet tykadel: "))
-
-if pocet_tykadel > 12:
-    print("Odkud jste přiletěli?")
-if pocet_tykadel > 6:
-    print("Že by pavouček?")
-else:
-    print("Tak málo tykadel mě nezajímá!")
